d9680ac6b786233211782ab269ced781f3466340
Frederick999 <frederickbenavidez52@gmail.com>
2025-04-05 17:55:40 -0600

    .

--- Practica08/models/Task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const taskSchema = new mongoose.Schema({
-    titulo: { type: String, required: true },
-    descripcion: { type: String, required: false },
-    completada: { type: Boolean, default: false }
-}, {timestamps: true});
-
-module.exports = mongoose.model('Task', taskSchema)
